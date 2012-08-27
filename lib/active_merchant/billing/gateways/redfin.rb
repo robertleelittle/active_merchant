44e5beeb5aea08fd27c5930f3bbefb6cf8ec3acb
Robert Lee Little <little@mac.com>
2012-08-26 23:51:32 -0400

    updated the spliting of the response string.  Deleted the $ from the front and the back of the message then split by "$,$".

--- lib/active_merchant/billing/gateways/redfin.rb
+++ lib/active_merchant/billing/gateways/redfin.rb
@@ -4,46 +4,47 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class RedfinGateway < AuthorizeNetGateway
       self.live_url = self.test_url = 'https://payment.redfinnet.com/aim/transact.aspx'
-      
+
       self.homepage_url = 'http://www.redfinnet.com/'
       self.display_name = 'RedfinNetwork'
-      
+
       # Limit support to purchase() for the time being
       # JRuby chokes here
       # undef_method :authorize, :capture, :void, :credit
-      
+
       # undef_method :authorize
       # undef_method :capture
       # undef_method :void
       # undef_method :credit
-      
+
       def test?
         Base.gateway_mode == :test
       end
-      
+
       private
-      
-      
+
+
       def success?(response)
-        response[:response_code].to_i == APPROVED
+        response[:response_code] == APPROVED
       end
 
       def fraud_review?(response)
-        response[:response_code].to_i == FRAUD_REVIEW
+        response[:response_code] == FRAUD_REVIEW
       end
 
-      
-      
+
+
       def split(response)
         puts("Response Text: #{response}")
-        response.split('$,$')
+        response[1,(response.length-2)].split("$,$")
+        # response.split('$,$')
       end
-      
+
       def parse(body)
         fields = split(body)
 
         results = {
-          :response_code => fields[RESPONSE_CODE],
+          :response_code => fields[RESPONSE_CODE].to_i,
           :response_reason_code => fields[RESPONSE_REASON_CODE],
           :response_reason_text => fields[RESPONSE_REASON_TEXT],
           :avs_result_code => fields[AVS_RESULT_CODE],
