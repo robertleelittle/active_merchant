81c0524ad7e7fe49c88dc3d62a06cdb3a8a57da5
Robert Lee Little <little@mac.com>
2012-08-26 20:08:26 -0400

    added redfin gateway to customized gem file.

---
+++ lib/active_merchant/billing/gateways/redfin_gateway.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/authorize_net'
+
+module ActiveMerchant #:nodoc:
+  module Billing #:nodoc:
+    class RedfinGateway < AuthorizeNetGateway
+      self.live_url = self.test_url = 'https://payment.redfinnet.com/aim/transact.aspx'
+      
+      self.homepage_url = 'http://www.redfinnet.com/'
+      self.display_name = 'RedfinNetwork'
+      
+      # Limit support to purchase() for the time being
+      # JRuby chokes here
+      # undef_method :authorize, :capture, :void, :credit
+      
+      undef_method :authorize
+      undef_method :capture
+      undef_method :void
+      undef_method :credit
+      
+      def test?
+        Base.gateway_mode == :test
+      end
+      
+      private
+      def split(response)
+        response.split(',')
+      end
+    end
+  end
+end
